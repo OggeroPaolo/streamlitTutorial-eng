438ef630d8b6ffc1832df59aa4cbfa2a5f49162d
OggeroPaolo <133958035+OggeroPaolo@users.noreply.github.com>
2023-05-18 15:07:08 +0200

    V2

--- pages/1_📈_Analysis.py
+++ pages/1_📈_Analysis.py
@@ -13,6 +13,20 @@ def create_products_tab(products_tab):
     col2.metric('Max Payment', f"$ {compact_format(payment_info_dict[0]['Max Payment'])}")
     col3.metric('Average Payment', f"$ {compact_format(payment_info_dict[0]['Average Payment'])}")
 
+    with products_tab.expander("Product Overview",True):
+        prod_col1, prod_col2, prod_col3 = st.columns([3, 3, 4])
+        sort_par = prod_col1.radio("sorted by: ", ["code", "name", "quantity", "price"])
+        sort_choice = prod_col2.selectbox("Ordered by: ", ["ascending", "descending"])
+
+        sort_dict ={"ascending": "ASC", "descending": "DESC"}
+
+        if prod_col1.button("Sort", type="primary"):
+            query1 = "SELECT productCode AS 'code', productName AS 'name', quantityInStock AS 'quantity', buyPrice AS 'price', MSRP FROM products"
+            query2 = f"ORDER BY {sort_par} {sort_dict[sort_choice]}"
+            products = execute_query(st.session_state["connection"], query1+ " " + query2 + ";")
+            df_products = pd.DataFrame(products)
+            st.dataframe(df_products, use_container_width=True)
+
 # each tab has a separate function
 
 if __name__ == "__main__":
